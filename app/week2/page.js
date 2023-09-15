06bf48eb90b9756c3b57d9ebc0141ab39a6ccab8
David Adesegun <david.adesegun2@gmail.com>
2023-09-14 21:50:42 -0600

    Restore accidentally deleted component

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,1 +1,12 @@
+import StudentInfo from '../studentInfo';
 
+const ShoppingList = () => {
+    return (
+        <div>
+            <h1>My Shopping List</h1>
+            <StudentInfo />
+        </div>
+    );
+};
+
+export default ShoppingList;
