a764c50a6687a680c3d29c29ae9446f4ed24c0f7
David Adesegun <david.adesegun2@gmail.com>
2023-09-14 21:46:43 -0600

    Create StudentInfo, Home, ShoppingList components

---
+++ app/studentInfo.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h2>David Adesegun</h2>
+            <p>CPRG 306 F</p>
+            <Link href="https://github.com/david-adesegun/cprg306-assignments">GitHub Repository</Link>
+        </div>
+    );
+};
+
+export default StudentInfo;
