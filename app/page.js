b8233da573eef7019a0bd57ab0b26229a1ce1bc7
David Adesegun <david.adesegun2@gmail.com>
2023-10-08 15:03:48 -0600

    Created form submission of grocery items in week 4

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
       <Link href="/week2">Week 2 </Link>
-      <Link href="/week3">Week 3</Link>
+      <Link href="/week3">Week 3 </Link>
+      <Link href="/week4">Week 4</Link>
     </div>
   );
 }
